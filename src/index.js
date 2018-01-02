c88f833108ca6d6fa5bc8930c243aaf393e49d0e
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-02 21:37:29 +0200

    Add flexboxgrid2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./fonts.css";
+import "flexboxgrid2";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
