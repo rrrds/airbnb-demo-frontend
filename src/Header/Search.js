64aa93d484b8200d4d30eabda07c649b36751bcc
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-05 00:04:11 +0200

    make searchbox wider

--- src/Header/Search.js
+++ src/Header/Search.js
@@ -5,6 +5,7 @@ import searchIcon from "./search.svg";
 
 const Input = styled.input`
   height: 48px;
+  width: 100%;
 
   background: #ffffff;
   border: 1px solid rgba(72, 72, 72, 0.2);
@@ -25,6 +26,7 @@ const Input = styled.input`
 
 const Label = styled.label`
   position: relative;
+  width: 100%;
 
   &:before {
     position: absolute;
