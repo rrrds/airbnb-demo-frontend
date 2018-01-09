51c3ab87d65327d2dbcc79961e66f0aa4c70f15f
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-10 01:47:47 +0200

    config simple BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./Landing";
+import Homes from "./Homes";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Landing />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/" exect component={Landing} />
+          <Route path="/homes" component={Homes} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
