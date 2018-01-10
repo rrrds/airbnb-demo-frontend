aa242e3db7c6d49374aec49997b69b930f4f8bc7
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-10 10:54:20 +0200

    add fixed warpper for filters

--- src/Homes/index.js
+++ src/Homes/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import { Grid } from "react-flexbox-grid";
 import Header from "../Header";
+import { FixedWrapper } from "../UI/styled";
 
 const Content = styled(Grid)`
-  padding-top: 80px;
+  padding-top: 136px;
+`;
+
+const FiltersWrapper = FixedWrapper.extend`
+  top: 81px;
 `;
 
 export default () => {
   return (
     <div>
+      <FiltersWrapper>Filters</FiltersWrapper>
       <Content>
         <div>Homes</div>
       </Content>
