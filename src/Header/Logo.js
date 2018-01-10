c486d98ec0e08650f0725f981e2b46252d05cdea
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-10 21:51:39 +0200

    make logo as link to mainpage

--- src/Header/Logo.js
+++ src/Header/Logo.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "./logo.svg";
 
 export default function() {
-  return <img src={logo} alt="logo" />;
+  return (
+    <Link to="/">
+      <img src={logo} alt="logo" />
+    </Link>
+  );
 }
