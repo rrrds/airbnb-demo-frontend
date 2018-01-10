ae6d0d0414289dd3923d459b27a3c73f34a56f76
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-10 23:02:29 +0200

    add space to main logo

--- src/Header/Logo.js
+++ src/Header/Logo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import logo from "./logo.svg";
+
+const MainLink = styled(Link)`
+  margin-right: 3vw;
+`;
 
 export default function() {
   return (
-    <Link to="/">
+    <MainLink to="/">
       <img src={logo} alt="logo" />
-    </Link>
+    </MainLink>
   );
 }
