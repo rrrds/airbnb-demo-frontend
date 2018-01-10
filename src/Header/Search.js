b38c067010f188d0ec063546afe5a372aab8bcb8
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-10 23:04:00 +0200

    update size of searchbar

--- src/Header/Search.js
+++ src/Header/Search.js
@@ -6,6 +6,7 @@ import searchIcon from "./search.svg";
 const Input = styled.input`
   height: 48px;
   width: 100%;
+  max-width: 392px;
 
   background: #ffffff;
   border: 1px solid rgba(72, 72, 72, 0.2);
