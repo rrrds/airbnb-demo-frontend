1e5586a0f51319386eb45c8e9707786e8055a42b
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-11 00:11:46 +0200

    show simple DayPicker using state

--- src/Homes/Filters.js
+++ src/Homes/Filters.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { Row, Col, Grid } from "react-flexbox-grid";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
 
 const Button = styled.button`
   border: 1px solid rgba(72, 72, 72, 0.2);
@@ -16,13 +18,43 @@ const Button = styled.button`
   color: #383838;
 `;
 
+const DayPickerOverlayWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const DayPickerWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 15px;
+  background: #fff;
+`;
+
 class Filters extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleOpen = e => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
+
   render() {
     return (
       <Grid>
         <Row middle="xs" start="xs">
           <Col xs={12}>
-            <Button>Dates</Button>
+            {this.state.isOpen && (
+              <DayPickerOverlayWrapper>
+                <DayPickerWrapper>
+                  <DayPicker />
+                </DayPickerWrapper>
+              </DayPickerOverlayWrapper>
+            )}
+            <Button onClick={this.toggleOpen}>
+              {this.state.isOpen ? "Check in — Check out" : "Dates"}
+            </Button>
+
             <Button>Guests</Button>
             <Button>Room type</Button>
             <Button>Price</Button>
