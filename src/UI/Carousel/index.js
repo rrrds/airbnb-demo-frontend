bf2447e689c751d5f31aed8e24002b0afccca549
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-11 14:14:07 +0200

    move Arrow src to Carousel

--- src/UI/Carousel/index.js
+++ src/UI/Carousel/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { CarouselArrow } from "../../UI/styled";
+import arrowRight from "./arrow-right.svg";
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,11 +15,33 @@ const Items = styled.div`
   overflow-x: auto;
 `;
 
+const Arrow = styled.a`
+  position: absolute;
+  right: -5px;
+  align-self: center;
+  background-color: white;
+  background-image: url(${arrowRight});
+  background-repeat: no-repeat;
+  background-size: 10px 18px;
+  background-position: center center;
+  border: 0.5px solid rgba(72, 72, 72, 0.2);
+  box-sizing: border-box;
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.16);
+  border-radius: 20px;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+
+  @media only screen and (max-width: 991px) {
+    display: none;
+  }
+`;
+
 export default function(props) {
   return (
     <Wrapper>
       <Items>{props.children}</Items>
-      {props.showArrow === true && <CarouselArrow />}
+      {props.showArrow === true && <Arrow />}
     </Wrapper>
   );
 }
--- src/UI/styled.js
+++ src/UI/styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { Row } from "react-flexbox-grid";
 import { Link } from "react-router-dom";
-import arrowRight from "../assets/arrow-right.svg";
 
 export const TitleH2 = styled.h2`
   line-height: 34px;
@@ -19,28 +18,6 @@ export const CarouselRow = styled(Row)`
   position: relative;
   flex-wrap: nowrap;
   overflow-x: auto;
-`;
-
-export const CarouselArrow = styled.a`
-  position: absolute;
-  top: 40%;
-  right: -5px;
-  background-color: white;
-  background-image: url(${arrowRight});
-  background-repeat: no-repeat;
-  background-size: 10px 18px;
-  background-position: center center;
-  border: 0.5px solid rgba(72, 72, 72, 0.2);
-  box-sizing: border-box;
-  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.16);
-  border-radius: 20px;
-  cursor: pointer;
-  width: 40px;
-  height: 40px;
-
-  @media only screen and (max-width: 991px) {
-    display: none;
-  }
 `;
 
 export const CardLink = styled.a`
