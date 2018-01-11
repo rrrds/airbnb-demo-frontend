8a76f128921729f521dcf67e27dcd68f2f6e92fe
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-11 04:51:18 +0200

    update arrow in main logo

--- src/Header/Logo.js
+++ src/Header/Logo.js
@@ -2,15 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import logo from "./logo.svg";
+import arrow from "./arrow-down.svg";
 
-const MainLink = styled(Link)`
+const Wrapper = styled.div`
+  display: flex;
+  content-align: center;
   margin-right: 3vw;
+`;
+
+const Arrow = styled.button`
+  border: none;
+  background: url(${arrow}) center center no-repeat / auto;
+  margin-left: 5px;
 `;
 
 export default function() {
   return (
-    <MainLink to="/">
-      <img src={logo} alt="logo" />
-    </MainLink>
+    <Wrapper>
+      <Link to="/">
+        <img src={logo} alt="logo" />
+      </Link>
+      <Arrow className="hidden-lg hidden-xl" />
+    </Wrapper>
   );
 }
