362f99a995180c05ef30d1f3d604eb370ba54e9e
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-13 03:09:49 +0200

    add common components

--- src/Homes/styled.js
+++ src/Homes/styled.js
@@ -16,3 +16,44 @@ export const RoundButton = styled.button`
     border: 1px solid #008489;
   }
 `;
+
+export const FilterButton = styled.button`
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+
+  padding: 7px 16px;
+  margin: 12px 12px 12px 0;
+  background-color: ${props => (props.active ? "#008489" : "#fff")};
+
+  line-height: normal;
+  font-size: 14px;
+  color: ${props => (props.active ? "#fff" : "#383838")};
+`;
+
+export const PopupOverlayWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+
+  :before {
+    content: "";
+    position: fixed;
+    background: #fff;
+    mix-blend-mode: normal;
+    opacity: 0.8;
+    height: 100vh;
+    width: 100vw;
+    z-index: -50;
+  }
+`;
+
+export const PopupWrapper = styled.div`
+  position: absolute;
+  left: -1px;
+  top: 15px;
+  background: #fff;
+  z-index: 20;
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
+  border-radius: 4px;
+`;
