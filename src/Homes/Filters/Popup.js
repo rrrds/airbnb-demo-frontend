627280216ee4bd55543dcca2aeae14717f7e584b
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-13 20:33:20 +0200

    init Popup with clickOutside handler

---
+++ src/Homes/Filters/Popup.js
@@ -0,0 +1,10 @@
+import onClickOutside from "react-onclickoutside";
+import { PopupWrapper } from "../styled";
+
+class Popup extends PopupWrapper {
+  handleClickOutside = e => {
+    this.props.handleClickOutside();
+  };
+}
+
+export default onClickOutside(Popup);
