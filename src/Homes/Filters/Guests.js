2218ec40008836f2be7ed6288553c01729da437b
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-13 03:56:47 +0200

    update Guest layout

--- src/Homes/Filters/Guests.js
+++ src/Homes/Filters/Guests.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import {
   RoundButton,
+  PopupButton,
   FilterButton,
   PopupOverlayWrapper,
   PopupWrapper
@@ -14,18 +15,40 @@ const Wrapper = styled.div`
   display: inline-block;
 `;
 
-const DayPickerBbar = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 22px 22px 22px;
+const SpacedPopupWrapper = PopupWrapper.extend`
+  padding: 24px 15px 0 24px;
 `;
 
-const PickerButton = styled.a`
-  line-height: normal;
-  font-size: 16px;
-  text-align: center;
+const TypeRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 23px;
+`;
 
-  color: ${props => (props.primary ? "#0F7276" : "#636363")};
+const ActionRow = TypeRow.extend`
+  margin: 0 0 0 85px;
+
+  > span {
+    min-width: 50px;
+    text-align: center;
+  }
+`;
+
+const Name = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+  line-height: normal;
+  font-size: 20px;
+  color: #383838;
+
+  > small {
+    font-size: 16px;
+    margin-top: 7px;
+  }
 `;
 
 class Guests extends React.Component {
@@ -57,46 +80,63 @@ class Guests extends React.Component {
       <Wrapper>
         {this.props.activeFilter === FILTER_ID && (
           <PopupOverlayWrapper>
-            <PopupWrapper>
-              <Grid>
-                <Row>
-                  <Col>Adults</Col>
-                  <Col>
-                    <RoundButton onClick={e => this.onDecrase("adults", e)}>
-                      -
-                    </RoundButton>
-                    <span>{this.state.adults}</span>
-                    <RoundButton onClick={e => this.onIncrease("adults", e)}>
-                      +
-                    </RoundButton>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>Children</Col>
-                  <Col>
-                    <RoundButton onClick={e => this.onDecrase("children", e)}>
-                      -
-                    </RoundButton>
-                    <span>{this.state.children}</span>
-                    <RoundButton onClick={e => this.onIncrease("children", e)}>
-                      +
-                    </RoundButton>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>Infants</Col>
-                  <Col>
-                    <RoundButton onClick={e => this.onDecrase("infants", e)}>
-                      -
-                    </RoundButton>
-                    <span>{this.state.infants}</span>
-                    <RoundButton onClick={e => this.onIncrease("infants", e)}>
-                      +
-                    </RoundButton>
-                  </Col>
-                </Row>
-              </Grid>
-            </PopupWrapper>
+            <SpacedPopupWrapper>
+              <TypeRow>
+                <Name>Adults</Name>
+                <ActionRow>
+                  <RoundButton
+                    onClick={e => this.onDecrase("adults", e)}
+                    disabled={this.state.adults < 1}
+                  >
+                    -
+                  </RoundButton>
+                  <span>{this.state.adults}</span>
+                  <RoundButton onClick={e => this.onIncrease("adults", e)}>
+                    +
+                  </RoundButton>
+                </ActionRow>
+              </TypeRow>
+
+              <TypeRow>
+                <Name>
+                  Children<small>Ages 2 — 12</small>
+                </Name>
+                <ActionRow>
+                  <RoundButton
+                    onClick={e => this.onDecrase("children", e)}
+                    disabled={this.state.children < 1}
+                  >
+                    -
+                  </RoundButton>
+                  <span>{this.state.children}</span>
+                  <RoundButton onClick={e => this.onIncrease("children", e)}>
+                    +
+                  </RoundButton>
+                </ActionRow>
+              </TypeRow>
+
+              <TypeRow>
+                <Name>
+                  Infants<small>Under 2</small>
+                </Name>
+                <ActionRow>
+                  <RoundButton
+                    onClick={e => this.onDecrase("infants", e)}
+                    disabled={this.state.infants < 1}
+                  >
+                    -
+                  </RoundButton>
+                  <span>{this.state.infants}</span>
+                  <RoundButton onClick={e => this.onIncrease("infants", e)}>
+                    +
+                  </RoundButton>
+                </ActionRow>
+              </TypeRow>
+              <TypeRow>
+                <PopupButton>Cancel</PopupButton>
+                <PopupButton primary>Apply</PopupButton>
+              </TypeRow>
+            </SpacedPopupWrapper>
           </PopupOverlayWrapper>
         )}
         <FilterButton
