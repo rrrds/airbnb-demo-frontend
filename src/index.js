f1771891c57423bbb033ecb8a9ee32d7911e32d2
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-23 11:38:08 +0200

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './fonts/fonts.css';
-import 'flexboxgrid2';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
