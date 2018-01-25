835072fea7111a2384a5200675b1f6a7e1b0feb6
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-26 01:36:02 +0200

    add styles to toggle button

--- src/Homes/Filters/InstantBook/index.js
+++ src/Homes/Filters/InstantBook/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import ToggleSwitch from 'react-toggle-switch';
 import { FilterButton } from '../../styled';
 import Dropdown from '../Dropdown';
 import ResponsivePopup from '../Dropdown/ResponsivePopup';
+import cross from './cross.svg';
+import check from './check.svg';
 
 const filterId = 'instantbook';
+
+const Switch = styled(ToggleSwitch)`
+  width: 64px;
+  min-width: 64px;
+  height: 40px;
+  background: rgba(72, 72, 72, 0.08);
+  border: 1px solid rgba(72, 72, 72, 0.3);
+  box-sizing: border-box;
+  border-radius: 24px;
+  cursor: pointer;
+  display: inline-block;
+
+  .switch-toggle {
+    top: -1px;
+    left: -1px;
+    position: relative;
+    transition: left 0.2s ease-in-out;
+
+    background: #ffffff url(${props => (props.on ? check : cross)}) center no-repeat;
+    border: 1px solid rgba(72, 72, 72, 0.3);
+    box-sizing: border-box;
+    border-radius: 20px;
+    width: 40px;
+    height: 40px;
+  }
+
+  &.on {
+    background: #008489;
+    border: 1px solid rgba(0, 132, 137, 0.3);
+  }
+
+  &.on .switch-toggle {
+    left: 23px;
+  }
+`;
 
 const SpacedPopupWrapper = styled.div`
   padding: 24px 16px 24px 16px;
@@ -22,6 +60,7 @@ const Name = styled.div`
   font-size: 18px;
   color: #383838;
   margin-bottom: 4px;
+  margin-right: 30px;
   min-width: 200px;
 `;
 
@@ -65,7 +104,10 @@ class InstantBook extends React.Component {
 
   render() {
     const Button = (
-      <FilterButton onClick={() => this.props.onButtonClick(filterId)} active={this.props.isActive}>
+      <FilterButton
+        onClick={() => this.props.onButtonClick(filterId)}
+        active={this.props.isActive || this.state.instantBook}
+      >
         Instant Book
       </FilterButton>
     );
@@ -85,11 +127,7 @@ class InstantBook extends React.Component {
                 Instant Book
                 <Description>Listings you can book without waiting for host approval.</Description>
               </Name>
-              <input
-                type="checkbox"
-                checked={this.state.instantBook}
-                onChange={this.handleCheckboxChange}
-              />
+              <Switch onClick={this.handleCheckboxChange} on={this.state.instantBook} />
             </Label>
           </SpacedPopupWrapper>
         </ResponsivePopup>
