1c9e26e3d6b330b966206093e0d4298ec5486947
Yura Kushnir <kushnir.yura@gmail.com>
2018-01-27 04:01:12 +0200

    use custom wrapper for large popup

--- src/Homes/Filters/Dropdown/LargePopup.js
+++ src/Homes/Filters/Dropdown/LargePopup.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import onClickOutside from 'react-onclickoutside';
 import { PopupButton, PopupOverlayWrapper } from '../../styled';
-import Popup from '../Popup';
 
 const ActionBbar = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 0 22px 22px 22px;
 `;
+
+const Wrapper = styled.div`
+  position: fixed;
+  left: 0;
+  top: 138px;
+  bottom: 0;
+  width: 66.6%;
+  background: #fff;
+  z-index: 20;
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const Popup = onClickOutside(Wrapper, {
+  handleClickOutside(instance) {
+    return instance.props.handleClickOutside;
+  },
+});
 
 export default props => (
   <PopupOverlayWrapper>
@@ -16,7 +36,7 @@ export default props => (
       <ActionBbar>
         <PopupButton onClick={props.onClose}>Cancel</PopupButton>
         <PopupButton onClick={props.onApply} primary>
-            Apply
+          Apply
         </PopupButton>
       </ActionBbar>
     </Popup>
