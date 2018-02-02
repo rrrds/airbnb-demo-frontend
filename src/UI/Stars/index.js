03c8bcf67bb9eed3b2aa764a155b5663e7a2f8c5
Yura Kushnir <kushnir.yura@gmail.com>
2018-02-02 14:27:54 +0200

    use lodash.times in stars

--- src/UI/Stars/index.js
+++ src/UI/Stars/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import times from 'lodash/times';
 import star from './star.svg';
 import starEmpty from './star_empty.svg';
 
@@ -18,9 +18,7 @@ export default function (props) {
   return (
     <Wrapper>
       {props.children !== false &&
-        [...Array(5)].map((e, i) => (
-          <StarImg key={i.toString()} src={i < props.children ? star : starEmpty} />
-        ))}
+        times(5, i => <StarImg key={i.toString()} src={i < props.children ? star : starEmpty} />)}
     </Wrapper>
   );
 }
