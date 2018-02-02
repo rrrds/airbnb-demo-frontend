63bdb61a9997f72905e356eeea9e901dbefb11f7
Yura Kushnir <kushnir.yura@gmail.com>
2018-02-02 10:28:44 +0200

    api

--- src/Landing/Homes/index.js
+++ src/Landing/Homes/index.js
@@ -4,53 +4,41 @@ import { TitleH2, HeaderRow } from '../../UI/styled';
 import SeeAllLink from '../../UI/SeeAllLink';
 import HomesList from '../../Homes/List';
 import Carousel from '../../UI/Carousel';
+import { transformHomeData } from '../../Homes/styled';
 
-import image1 from './1.png';
-import image2 from './2.png';
-import image3 from './3.png';
+const apiUrl = 'https://airbnb-demo-api.now.sh/v1/homes';
 
-const homes = [
-  {
-    image: image1,
-    text: 'La Salentina, see, nature & relax',
-    price: 82,
-    rating: 4,
-    host: '97 · Superhost',
-    type: 'Entire house',
-    beds: 9,
-  },
-  {
-    image: image2,
-    text: 'Your private 3 bedr. riad and exclusi…',
-    price: 82,
-    rating: 4,
-    host: '161 · Superhost',
-    type: 'Entire house',
-    beds: 5,
-  },
-  {
-    image: image3,
-    text: 'Dreamy Tropical Tree House',
-    price: 200,
-    rating: 5,
-    host: '364 · Superhost',
-    type: 'Entire treehouse',
-    beds: 1,
-  },
-];
+export default class Homes extends React.Component {
+  state = {
+    homes: [],
+  };
 
-export default function () {
-  return (
-    <div>
-      <HeaderRow between="xs" middle="xs">
-        <TitleH2>Homes</TitleH2>
-        <SeeAllLink to="/homes" />
-      </HeaderRow>
-      <Row>
-        <Carousel showArrow>
-          <HomesList homes={homes} xs={8} md={5} lg={4} />
-        </Carousel>
-      </Row>
-    </div>
-  );
+  componentWillMount = () => {
+    fetch(`${apiUrl}?limit=6&offset=0`)
+      .then(response => response.json())
+      .then((data) => {
+        const homes = data.items.map(i => transformHomeData(i));
+
+        this.setState({ homes });
+      })
+      .catch((ex) => {
+        console.log('error', ex);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <HeaderRow between="xs" middle="xs">
+          <TitleH2>Homes</TitleH2>
+          <SeeAllLink to="/homes" />
+        </HeaderRow>
+        <Row>
+          <Carousel showArrow>
+            <HomesList homes={this.state.homes} xs={8} md={5} lg={4} />
+          </Carousel>
+        </Row>
+      </div>
+    );
+  }
 }
