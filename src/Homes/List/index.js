9fa27d72721afa1e2bf685a6942b12d620528657
Yura Kushnir <kushnir.yura@gmail.com>
2018-02-02 14:22:10 +0200

    add skeleton provider with styles

--- src/Homes/List/index.js
+++ src/Homes/List/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Col } from 'react-flexbox-grid';
-
+import { createSkeletonProvider } from '@trainline/react-skeletor';
 import Card from '../Card';
+
+const SkeletonCard = createSkeletonProvider(
+  {
+    home: {
+      text: '______________ _ _ __ _ ______',
+      price: '__',
+      stars: false,
+      beds: 0,
+      type: '________',
+      host: '__ _______',
+    },
+  },
+  ({ home }) => home === undefined,
+  () => ({
+    color: 'grey',
+    backgroundColor: 'grey',
+    borderRadius: '4px',
+    opacity: 0.3,
+    width: 'max-content',
+  }),
+)(Card);
 
 export default function (props) {
   return props.homes.map((home, index) => (
     <Col key={index.toString()} xs={props.xs} md={props.md} lg={props.lg}>
-      <Card home={home} />
+      <SkeletonCard home={home} />
     </Col>
   ));
 }
