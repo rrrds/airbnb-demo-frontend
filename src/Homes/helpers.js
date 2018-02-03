c3086cfb7b8ac271f8dbcb74299f98e0af4a5b09
Yura Kushnir <kushnir.yura@gmail.com>
2018-02-03 15:51:47 +0200

    create helpers functions

---
+++ src/Homes/helpers.js
@@ -0,0 +1,22 @@
+const formatHost = (reviewsCount, isSuperhost) => {
+  if (isSuperhost) {
+    return `${reviewsCount} · Superhost`;
+  }
+
+  return reviewsCount;
+};
+
+export const kindToText = {
+  entire_home: 'Entire home',
+  private_room: 'Private room',
+};
+
+export const transformHomeData = inData => ({
+  text: inData.name,
+  image: inData.images[0].picture,
+  price: inData.price,
+  stars: inData.rating,
+  beds: inData.bedsCount,
+  type: kindToText[inData.kind],
+  host: formatHost(inData.reviewsCount, inData.isSuperhost),
+});
