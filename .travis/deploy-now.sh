00afb47e9346a99d8defdac763580c61d0418c99
Yura Kushnir <kushnir.yura@gmail.com>
2018-02-06 10:30:10 +0200

    update deploy script

--- .travis/deploy-now.sh
+++ .travis/deploy-now.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 set -ev
+
+DEPLOYMENT_URL=$(now deploy \
+    --public \
+    --static \
+    --name airbnb-master \
+    --token="${NOW_TOKEN}" "${TRAVIS_BUILD_DIR}/build")
+
+now alias set ${DEPLOYMENT_URL} ${NOW_ALIAS} --token="${NOW_TOKEN}"
